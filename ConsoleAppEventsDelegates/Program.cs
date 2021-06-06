37d87760d12614c56d126f08da043abf9133415e
farazz91 <farazz91@gmail.com>
2021-06-06 18:55:57 +0530

    added event and delegate

--- ConsoleAppEventsDelegates/Program.cs
+++ ConsoleAppEventsDelegates/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleAppEventsDelegates
 {
     class Program
     {
+        public delegate void VideoEncodeEventHandlerDelegate (object Source,EventArgs argss);    //delegate
+
+        public event VideoEncodeEventHandlerDelegate videoEncoded;  //event of above delegate
+
+        protected virtual void OnVideoEncoded()
+        {
+            if(videoEncoded!=null)
+            {
+                videoEncoded(this, EventArgs.Empty);
+            }
+        }
+        public void Encode()
+        {
+            Console.WriteLine("Encoding vedio...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
